a19ed6a91bbe5031d90c788ea799e970aa883183
Cassie  West <cjwest0727@gmail.com>
2022-01-26 13:18:33 -0700

    filereader/writer

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,22 @@
+import java.io.*;
+
 public class Driver {
+    public String[][] fileFormatted;
+
+
 
     public static void main(String[] args) {
-        System.out.println("hi");
-        System.out.println("hi");
+
+
+
     }
 
+    public void createFileReader(String imagePath) throws FileNotFoundException {
 
-
+        FileReader input = new FileReader((imagePath));
+    }
+    public void stringsToArray1() throws IOException {
+        FileWriter myWriter1 = new FileWriter("test.txt");
+    }
 
 }

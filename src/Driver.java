aed0f14cc4289a70090c4f56319d05208f28df9b
bradfan <bradley.tomlinson@genspark.com>
2022-01-26 09:59:01 -0700

    Add txt files

--- src/Driver.java
+++ src/Driver.java
@@ -2,15 +2,10 @@ public class Driver {
 
 
 
-
-
-
     public static void main(String[] args) {
-        System.out.println("hi");
 
-        System.out.println("goodbye");
 
-        System.out.println("hi");
+
     }
 
 

91a2eba9fc7966b8134e67f03919152530a37bbd
bradfan <bradley.tomlinson@genspark.com>
2022-01-28 12:26:23 -0700

    Test complete on firstProblem Method, TeamFileTest

--- src/main/Test/TeamFileTest.java
+++ src/main/Test/TeamFileTest.java
@@ -20,23 +20,28 @@ class TeamFileTest {
     @org.junit.jupiter.api.Test
     void firstProblem() throws IOException {
         ArrayList<String> first = new ArrayList<>();
-        first.add("One, Five");
+        first.add("One");
+        first.add("Five");
         ArrayList<String> second = new ArrayList<>();
-        second.add("Two, Six");
+        second.add("Two");
+        second.add("Six");
         ArrayList<String> third = new ArrayList<>();
-        third.add("Three, Seven");
+        third.add("Three");
+        third.add("Seven");
         ArrayList<String> fourth = new ArrayList<>();
-        fourth.add("Four, ");
+        fourth.add("Four");
+        fourth.add("");
         ArrayList<ArrayList<String>> testArray = new ArrayList<>();
         testArray.add(first);
         testArray.add(second);
         testArray.add(third);
         testArray.add(fourth);
-        assertEquals(testArray, file.firstProblem(), "Doesn't match.");
+        assertEquals(testArray, file.firstProblem(),"Doesn't match.");
     }
 
     @org.junit.jupiter.api.Test
     void secondProblem() {
+
     }
 
 

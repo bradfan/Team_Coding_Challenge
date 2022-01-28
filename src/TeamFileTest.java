682f3087317acb70744376e1413a12d8dec933ea
bradfan <bradley.tomlinson@genspark.com>
2022-01-28 08:27:27 -0700

    Add Test Class

---
+++ src/TeamFileTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamFileTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void firstProblem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generateTextFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void secondProblem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generateSecondTextFile() {
+    }
+}
